df1ae83b81d4e0a09e4ee1b4be7ec4e0ec47cea8
Aleksandr <accgh4gb@gmail.com>
2022-05-21 21:41:37 +0300

    The first and fourth tasks of lesson 3

--- 01_Lesson_03/MainCPP_Lesson_3.cpp
+++ 01_Lesson_03/MainCPP_Lesson_3.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+extern const int a;
+extern const int b;
+extern const int c;
+extern const int d;
+
 int main()
 {
+	// Написать программу, вычисляющую выражение a * (b + (c / d))
+	// и выводящую результат с плавающей точкой, где a, b, c, d
+	// – целочисленные константы, объявленные и
+	// инициализированные в другом cpp файле. Используйте static_cast или
+	// С-Style cast к float чтобы выполнить точное деление. Используйте extern
+	double dResult = (a * (b + (static_cast<double>(c) / d))); 
+	std::cout << dResult << std::endl;
+
 	return 0;
 }
---
+++ 01_Lesson_03/data_for_Lesson_3.cpp
@@ -0,0 +1,5 @@
+
+extern const int a = 3;
+extern const int b = 22;
+extern const int c = 543;
+extern const int d = 7;
