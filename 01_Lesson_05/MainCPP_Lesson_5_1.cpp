51e4fcf8626abf233adc9d925982fafd3e1a6f3f
Aleksandr <accgh4gb@gmail.com>
2022-05-27 23:40:39 +0300

    The first task from the fifth lesson

--- 01_Lesson_05/MainCPP_Lesson_5_1.cpp
+++ 01_Lesson_05/MainCPP_Lesson_5_1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+void PrintArray(double *dArray, unsigned int N)
+{
+    for(size_t i = 0; i < N; i++)
+    {
+        std::cout << dArray[i] << " ";
+    }
+    
+    std::cout << std::endl;
+}
+
+
 int main()
 {
+    const unsigned int N = 5;
+    double dArray[N] = {3.4, 5.0, 2.75745345, 5.323, 745674.2342342};
+    PrintArray(dArray, N);
+    
     return 0;
 }
