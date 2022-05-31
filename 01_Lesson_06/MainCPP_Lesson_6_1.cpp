5d33506a5c0c03de1a6a615a9b178320b0e19ac4
Aleksandr <accgh4gb@gmail.com>
2022-06-01 00:34:42 +0300

    The first task from the sixth lesson

--- 01_Lesson_06/MainCPP_Lesson_6_1.cpp
+++ 01_Lesson_06/MainCPP_Lesson_6_1.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
+//Выделить в памяти динамический одномерный массив
+//типа int. Размер массива запросить у пользователя.
+//Инициализировать его числами степенями двойки: 1, 2, 4,
+//8, 16, 32, 64, 128 ... Вывести массив на экран. Не забыть
+//освободить память. =) Разбить программу на функции.
+
+void vInvite(size_t &iArraySize)
+{
+    std::cout << "Hello, dear user!" << std::endl;
+    std::cout << "Please, enter the value of array size: ";
+    std::cin >> iArraySize;
+}
+
+void vCreateDynamicArray(int *pArray, size_t &iArraySize)
+{
+    vInvite(iArraySize);
+
+    if(iArraySize <= 0)
+    {
+        throw "Wrong value";
+    }
+    else
+    {
+        pArray = new int [iArraySize];
+    }
+}
+
+
 int main()
 {
+    int * pArray;
+    size_t iArraySize = 0;
+    
+    vCreateDynamicArray(pArray, iArraySize);   
+    
+    for (size_t i = 0; i < iArraySize; ++i)
+    {
+        pArray[i] = 1 << i;
+    }
+
+    for (size_t i = 0; i < iArraySize; ++i)
+    {
+        std::cout << pArray[i] << " ";
+    } 
+    std::cout << std::endl; 
+    
+    delete[] pArray;
+    pArray = nullptr; 
     return 0;
 }
