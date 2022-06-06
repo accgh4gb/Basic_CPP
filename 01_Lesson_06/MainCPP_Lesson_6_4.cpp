cdbb04e2f880f842452509bf939742ade2201e8d
Aleksandr <accgh4gb@gmail.com>
2022-06-06 22:55:40 +0300

    The code has been optimized

--- 01_Lesson_06/MainCPP_Lesson_6_4.cpp
+++ 01_Lesson_06/MainCPP_Lesson_6_4.cpp
@@ -6,70 +6,52 @@
 //предыдущем уроке в третий текстовый файл
 //(имя файлов спросить у пользователя).
 
+void CopyFile(std::ifstream& ReadFile, std::ofstream& WriteFile)
+{
+    std::string text;
 
+    while(!ReadFile.eof())
+    {
+        getline(ReadFile, text);
+        WriteFile << text << std::endl;
+    }
+}
  
 int main()
 {
-    std::ifstream ReadFile1;
     std::cout << "Hello, dear user!" << std::endl;
-    std::cout << "Please, enter the name of the first file" << std::endl;
-    std::string sFileName1 = "";
+    std::cout << "Please, enter the name of the first file for input" << std::endl;
+    std::string sFileName1;
     std::cin >> sFileName1;
+    std::ifstream ReadFile1(sFileName1 + ".txt");
     
-    ReadFile1.open(sFileName1 + ".txt");
     
-    std::ifstream ReadFile2;
-    std::cout << "Please, enter the name of the second file" << std::endl;
+    std::cout << "Please, enter the name of the second file for input" << std::endl;
     std::string sFileName2 = "";
     std::cin >> sFileName2;
-    ReadFile2.open(sFileName2 + ".txt");
+    std::ifstream ReadFile2(sFileName2 + ".txt");
 
-    std::ofstream WriteFile;
     std::cout << "Please, enter the name for source file" << std::endl;
     std::string sFileNameSource = "";
     std::cin >> sFileNameSource;
-    WriteFile.open(sFileNameSource + ".txt");
-    std::string sBuffer;
-    if (ReadFile1.is_open())
+    std::ofstream WriteFile(sFileNameSource + ".txt");
+
+    if (ReadFile1.is_open() && ReadFile2.is_open() && WriteFile.is_open())
     {
-        while (!ReadFile1.eof())
-        {
-            getline(ReadFile1, sBuffer);
-            //WriteFile.write(&sBuffer[0], strlen(&sBuffer[0]));
-            //WriteFile.put(' ');
-             WriteFile << sBuffer << std::endl;
-        
-        }
+        CopyFile(ReadFile1, WriteFile);
         ReadFile1.close();
-    
-    }
-    else
-    {
-        std::cout << "Could not open the file " << std::endl;
-    
-    }
- 
-    if (ReadFile2.is_open())
-    {
-        while (!ReadFile2.eof())
-        {
-            getline(ReadFile2, sBuffer);
-            //WriteFile.write(&sBuffer[0], strlen(&sBuffer[0]));
-             WriteFile << sBuffer << std::endl;
-        }
+
+        CopyFile(ReadFile2, WriteFile);
         ReadFile2.close();
-    
+
+        WriteFile.close();         
     }
     else
     {
         std::cout << "Could not open the file " << std::endl;
-    
     }
- 
-    WriteFile.close();
  
     system("pause");
  
-    
     return 0;
 }
