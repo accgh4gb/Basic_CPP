ffdfced9ff8b93b617d2fa62f591f3cca9c215b4
Aleksandr <accgh4gb@gmail.com>
2022-06-13 00:23:45 +0300

    The file was modified a little

--- 01_Lesson_08/MyGame.cpp
+++ 01_Lesson_08/MyGame.cpp
@@ -54,7 +54,7 @@ int main()
                 break;
             }
             default:
-                printw("TiCTacToe");
+                printw("Please, try again.");
                 break;
         }
     }
